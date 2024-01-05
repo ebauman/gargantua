50c4e8957fa039663019ab3cf7181d51cd3de7f1
Eamon Bauman <eamon@eamonbauman.com>
2024-01-05 10:47:58 -0600

    openapi generation

--- generate.go
+++ generate.go
@@ -1,3 +1,6 @@
-//go:generate go run ./pkg/codegen crds ./artifacts/typedCRD/crd.yaml
-
+//go:generate go run k8s.io/kube-openapi/cmd/openapi-gen -i github.com/hobbyfarm/gargantua/v3/pkg/apis/hobbyfarm.io/v1,github.com/hobbyfarm/gargantua/v3/pkg/apis/hobbyfarm.io/v2,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/apimachinery/pkg/runtime,k8s.io/apimachinery/pkg/version,k8s.io/apimachinery/pkg/api/resource,k8s.io/api/core/v1,k8s.io/api/rbac/v1,k8s.io/apimachinery/pkg/util/intstr -o ./  -p /pkg/openapi -h hack/boilerplate.go.txt
 package main
+
+import (
+	_ "k8s.io/kube-openapi/cmd/openapi-gen/args"
+)
